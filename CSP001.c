4530016bc8bec15aca406fd730739426798766a9
vanhung272 <59682942+vanhung272@users.noreply.github.com>
2020-01-09 14:48:18 +0700

    Create CSP001.c

---
+++ CSP001.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+	char str[100];
+	int i,key;
+	do
+	{
+		printf("\nInput string: ");
+		gets(str);
+		
+		for(i=strlen(str);i>=0;i--)
+		{
+			if (str[i]==' ')
+			{
+				str[i]='\0';
+				printf("%s ",&str[i]+1);
+			}
+		}
+		printf("%s",str);
+		key=getch();
+		}	
+		while (key!=27);
+}
+
