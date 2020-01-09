d352fc4096ed1a70ebeab0c7974332ec32b2bad6
vanhung272 <59682942+vanhung272@users.noreply.github.com>
2020-01-09 15:24:37 +0700

    Create CSP.0001.c

---
+++ CSP.0001.c
@@ -0,0 +1,28 @@
+//Nguyen Van Hung se1406
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+	char str[100];
+	int i,key;
+	do
+	{
+		printf("\nInput a string: ");
+		gets(str);
+		printf("\nPrint old string:%s",str);
+			for(i=strlen(str);i>=0;i--)
+		{
+			if (str[i]==' ')
+			{
+				str[i]='\0';
+				printf("\nReverse String:%s ",&(str[i])+1);
+			}
+		}
+		printf("%s",str);
+		key=getch();
+		}	
+		while (key!=27);
+}
+
